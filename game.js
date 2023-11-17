0c7a9d6bdad0962a3bd44a445d763fb9b2257972
myloni <148680295+myloni@users.noreply.github.com>
2023-11-18 00:08:00 +0500

    Delete game.js

--- game.js
+++
@@ -1,9 +0,0 @@
-let count = 0;
-
-const clicker = document.getElementById('clicker');
-const countDisplay = document.getElementById('count');
-
-clicker.addEventListener('click', () => {
-    count++;
-    countDisplay.textContent = count;
-});
